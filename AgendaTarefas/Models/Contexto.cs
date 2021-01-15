486392b47c940f28e0879fa22fb39884d438a88f
Elioenai Oliveira Costa <elioenai.tecnologo@gmail.com>
2021-01-15 11:51:12 -0300

    Terceiro commit

---
+++ AgendaTarefas/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaTarefas.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+    }
+}
